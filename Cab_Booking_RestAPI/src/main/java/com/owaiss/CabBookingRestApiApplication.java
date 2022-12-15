b2fe66398d62d3b067dfacbbf59b897d1109602d
OwaissHassan <khabarquss@gmail.com>
2022-12-15 20:33:31 +0530

    swagger added

--- Cab_Booking_RestAPI/src/main/java/com/owaiss/CabBookingRestApiApplication.java
+++ Cab_Booking_RestAPI/src/main/java/com/owaiss/CabBookingRestApiApplication.java
@@ -3,7 +3,10 @@ package com.owaiss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CabBookingRestApiApplication {
 
 	public static void main(String[] args) {
